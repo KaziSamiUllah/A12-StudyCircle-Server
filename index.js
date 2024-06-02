0233db4b4cce73e44ec0807677370338c1bd50b4
KaziSamiUllah <samiullahsagor@gmail.com>
2024-06-02 23:00:41 +0600

    added material routes

--- index.js
+++ index.js
@@ -19,8 +19,7 @@ app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
 
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.unfglor.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.unfglor.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -33,13 +32,12 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-
-
     /////////////////////user APIs////////////////////
     const usersCollection = client.db("StudyCircle").collection("users");
 
     app.post("/users", async (req, res) => {
       const user = req.body;
+      no;
       const result = await usersCollection.insertOne(user);
       res.send(result);
     });
@@ -51,47 +49,71 @@ async function run() {
       res.send(user);
     });
 
+    ///////////////////sessions and materials APIs/////////////////////
 
-///////////////////sessions and materials APIs/////////////////////
+    const sessionCollection = client.db("StudyCircle").collection("sessions");
 
-const sessionCollection = client.db("StudyCircle").collection("sessions");
+    app.post("/sessions", async (req, res) => {
+      const session = req.body;
+      const result = await sessionCollection.insertOne(session);
+      res.send(result);
+    });
 
-app.post("/sessions", async (req, res) => {
-  const session = req.body;
-  const result = await sessionCollection.insertOne(session);
-  res.send(result);
-});
+    app.get("/sessions", async (req, res) => {
+      const data = sessionCollection.find();
+      const result = await data.toArray();
+      res.send(result);
+    });
 
-app.get("/sessions", async (req, res) => {
-  const data = sessionCollection.find();
-  const result = await data.toArray();
-  res.send(result);
-});
+    app.get("/sessionsdata/:id", async (req, res) => {
+      const ID = req.params.id;
+      const query = { _id: new ObjectId(ID) };
+      const sessionData = await sessionCollection.findOne(query);
+      res.send(sessionData);
+    });
 
-app.get("/sessions/:email", async (req, res) => {
-  const email = req.params.email;
-  const query = { tutorEmail: email };
-  const tutorSessions = await sessionCollection.find(query).toArray();
-  res.send(tutorSessions);
-});
+    app.get("/sessions/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { tutorEmail: email };
+      const tutorSessions = await sessionCollection.find(query).toArray();
+      res.send(tutorSessions);
+    });
 
+    app.put("/updateSessions/:id", async (req, res) => {
+      const data = req.body;
+      const paramsId = req.params.id;
+      console.log(data);
+      const filter = { _id: new ObjectId(paramsId) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          materials: {
+            "materialTitle": data.materialTitle,
+            "link": data.link,
+            "URL": data.URL,
+          },
+        },
+      };
+      const result = await sessionCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.send(result);
+    });
 
-app.delete("/sessions/:id", async (req, res) => {
-  const id = req.params.id;
-  const query = { _id: new ObjectId(id) };
-  const result = await sessionCollection.deleteOne(query);
-  res.send(result);
-});
-
-
-
-
-
-
+    app.delete("/sessions/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await sessionCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
