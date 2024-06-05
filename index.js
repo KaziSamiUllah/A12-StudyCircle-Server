16788b0216078e433a53b24c414d2ec333a20023
KaziSamiUllah <samiullahsagor@gmail.com>
2024-06-06 01:20:32 +0600

    final push for today

--- index.js
+++ index.js
@@ -70,6 +70,14 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      // const query = { _id: new ObjectId(id) };
+      // const result = await userCollection.deleteOne(query);
+      // res.send(result);
+    });
+
 
 
 
